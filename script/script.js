f35189758e9849433b0b73f3c720634b8b8d5dc4
Likhon-25 <likhonmondol327@gmail.com>
2026-03-07 07:37:17 +0600

    Categories Button added

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
-console.log("Js ADDED");
+
+
+const categoriesContainer = document.getElementById("categoriesContainer")
+
+async function loadCategories() {
+    // async await
+    const res = await fetch(
+        "https://openapi.programming-hero.com/api/categories"
+    );
+    const data = await res.json()
+    console.log(data);
+    console.log(categoriesContainer);
+    data.categories.forEach((category) => {
+        console.log(category);
+        const btn = document.createElement("button");
+        btn.className = "btn btn-outline w-full";
+        btn.textContent = category.categorie_name;
+        categoriesContainer.appendChild(btn)
+    });
+}
+loadCategories()
